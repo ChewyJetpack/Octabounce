f0d85c6a2c08c25acb3b20d15c08d585a83d8df4
Chewy Jetpack <chewyjetpack@gmail.com>
2019-12-15 00:10:54 +0000

    initial test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,41 @@
 import '../styles/index.scss';
+import WebMidi from "webmidi";
 
-console.log('webpack starterkit');
+WebMidi.enable(function (err) {
+    console.log('WebMidi is running');
+
+    const WebMidiSetup = () => {
+
+        // all midi inputs
+        const inputs = WebMidi.inputs;
+
+        console.log(inputs);
+
+        // create array of input names
+        const inputNames= [];
+        for (let input = 0; input < inputs.length; input++) {
+            inputNames.push(inputs[input]._midiInput.name);
+        }
+
+        console.log(inputNames);
+    };
+
+    // hard coded midi input - {todo} make this user-selectable
+    const input = WebMidi.getInputByName('Komplete Audio 6 MIDI');
+
+    // listen for 'play'
+    input.on('start', "all",
+        function (e) {
+        console.log("Go!");
+        }
+    );
+
+    // listen for 'stop
+    input.on('stop', "all",
+        function (e) {
+        console.log("Stop!");
+        }
+    );
+    
+    WebMidiSetup();
+});
