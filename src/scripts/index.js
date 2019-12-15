b7654be13f989b836793f28763944d00ee12226b
Chewy Jetpack <chewyjetpack@gmail.com>
2019-12-15 00:44:02 +0000

    logging out midi messages, filtering ticks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,20 +20,34 @@ WebMidi.enable(function (err) {
         console.log(inputNames);
     };
 
+    // record midi data
+    const recordMidi = () => {
+        const midiData = [];
+        
+    };
+
     // hard coded midi input - {todo} make this user-selectable
     const input = WebMidi.getInputByName('Komplete Audio 6 MIDI');
 
     // listen for 'play'
-    input.on('start', "all",
-        function (e) {
-        console.log("Go!");
+    input.on('start', "all", (e) => {
+            console.log("Go!");
         }
     );
 
+    // log out all received messages that are not clock messages (decimal 248)
+    input.on('midimessage', 'all', (e) => {
+        if (e.data.length > 1 || e.data[0] !== 248 ) {
+            console.log(e.data);
+        }
+    });
+
+    // need to write midi data to some kind of storage, ready for play back.
+    // https://github.com/grimmdude/MidiWriterJS
+
     // listen for 'stop
-    input.on('stop', "all",
-        function (e) {
-        console.log("Stop!");
+    input.on('stop', "all", (e) => {
+            console.log("Stop!");
         }
     );
     
